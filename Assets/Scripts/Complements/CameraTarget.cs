087a199be196b786e0bb68077d154954381de6f2
Guillermo Felix Romero <felix_50_15@live.com.ar>
2026-07-02 16:01:22 -0300

    ajuste de distancia

--- Assets/Scripts/Complements/CameraTarget.cs
+++ Assets/Scripts/Complements/CameraTarget.cs
@@ -2,16 +2,20 @@ using UnityEngine;
 
 public class CameraTarget : MonoBehaviour
 {
-    public Transform target, camera;
+    public Transform target;
+    [SerializeField] float lerpAmount = 0.1f;
+
+    Vector3 offset;
+    private void Start()
+    {
+        offset = target.position - transform.position;
+    }
 
 
 
-  
     // Update is called once per frame
-    void Update()
-{
-        Vector3 cameraTransform = new Vector3(target.position.x, camera.position.y, target.position.z);
-
-       camera.position = cameraTransform;
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Slerp(transform.position, target.position - offset, lerpAmount);
     }
 }
