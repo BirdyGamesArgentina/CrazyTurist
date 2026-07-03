cb00775490304b8ddccde6ad4f2fc49fc51c5202
SantiagoMinio <146845645+SantiagoMinioTrick@users.noreply.github.com>
2026-07-03 18:30:55 -0300

    Nueva end screen aunque le duela a fran

--- Assets/Scripts/UI/EndScreen.cs
+++ Assets/Scripts/UI/EndScreen.cs
@@ -1,6 +1,7 @@
 using Game.Scripts.Shared.Events;
 using Game.Scripts.Shared.ServiceLocator;
 using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,8 +11,12 @@ public class EndScreen : MonoBehaviour
     [SerializeField] private Button _resetButton;
     [SerializeField] private Button _backToMenuButton;
     [SerializeField] private TextMeshProUGUI _panelTitle;
+    [SerializeField] private TextMeshProUGUI _subText;
     [SerializeField] private TextMeshProUGUI _scoreText;
     [SerializeField] private UIPanelTransition _panelTransition;
+    [SerializeField] private UIPanelTransition _textPanelTransition;
+    [SerializeField, TextArea] private string loseText;
+    [SerializeField, TextArea] private string winText;
 
     private void Awake()
     {
@@ -25,20 +30,35 @@ public class EndScreen : MonoBehaviour
         _eventBus.Subscribe<OnEndScreenResult>(OnEndScreen);
     }
 
+    public void OpenOver()
+    {
+        Time.timeScale = 0;
+    }
+
     private void OnEndScreen(OnEndScreenResult result)
     {
         _panelTitle.text = result.Title;
         _scoreText.text = "Tu puntaje: " + result.Score;
+        _subText.text = result.Title == "Perdiste" ? loseText : winText;
         _panelTransition.Open();
+        StartCoroutine(WaitCoroutine());
+    }
+
+    IEnumerator WaitCoroutine()
+    {
+        yield return new WaitForSeconds(2);
+        _textPanelTransition.Open();
     }
 
     private void Btn_Reset()
     {
+        Time.timeScale = 1;
         SceneLoader.Load(1);
     }
 
     private void Btn_BackToMenu()
     {
+        Time.timeScale = 1;
         SceneLoader.Load(0);
     }
 }
