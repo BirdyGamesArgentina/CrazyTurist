9729cee10e0dbd3aa6feeefcce4066140581b82f
Guillermo Felix Romero <felix_50_15@live.com.ar>
2026-07-02 22:19:54 -0300

    colectivo iomplementado + Interest add y remove

--- Assets/Scripts/Complements/SliderLife.cs
+++ Assets/Scripts/Complements/SliderLife.cs
@@ -2,24 +2,52 @@ using UnityEngine;
 using UnityEngine.UI;
 public class SliderLife : MonoBehaviour
 {
-    [SerializeField] private Slider slider;
-    [SerializeField] private GameObject sliderFill;
+    public static SliderLife instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    [SerializeField] private Image img;
     [SerializeField] private float duration = 10f;
+
+    float interest = 100;
+    [SerializeField] int maxInterest = 100;
+
+    bool anim = false;
+    public void BeginCountDown()
+    {
+        anim = true;
+        interest = maxInterest;
+
+    }
 
     private void Start()
     {
-        slider.value = slider.maxValue;
+        img.fillAmount = 1f;
     }
 
     private void Update()
     {
-        slider.value -= (slider.maxValue / duration) * Time.deltaTime;
+        if (!anim) return;
+        interest = interest - 1 * Time.deltaTime;
+        Refresh();
+    }
 
-        if (slider.value <= 0)
-        {
-            slider.value = 0;
-            sliderFill.SetActive(false);
-            Debug.Log("eVento de perdiste");
-        }
+    void Refresh()
+    {
+        img.fillAmount = (float)interest / duration;
+    }
+
+    public void AddInterest(int toAdd)
+    {
+        interest += toAdd;
+        Refresh();
+    }
+
+    public void RemoveInterest(int toRemove)
+    {
+        interest -= toRemove;
+        Refresh();
     }
 }
