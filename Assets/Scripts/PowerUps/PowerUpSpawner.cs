22dcaee40417481b43cff41327e7e7200c061ef0
SantiagoMinio <146845645+SantiagoMinioTrick@users.noreply.github.com>
2026-07-04 03:43:50 -0300

    PowerUp Spawns

--- Assets/Scripts/PowerUps/PowerUpSpawner.cs
+++ Assets/Scripts/PowerUps/PowerUpSpawner.cs
@@ -2,9 +2,25 @@ using UnityEngine;
 
 public class PowerUpSpawner : MonoBehaviour
 {
+    [SerializeField] PowerUp[] possiblesPowerUps;
+    [SerializeField] float maxTimeSpawn;
+    [SerializeField] float minTimeSpawn;
+
+
+    private void Start()
+    {
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        var selected = possiblesPowerUps[Random.Range(0, possiblesPowerUps.Length)];
+        var current = Instantiate(selected, transform);
+        current.Spawn(this);
+    }
 
     public void DeletePowerUp(PowerUp powerUp)
     {
-
+        TimerManager.Instance.AddTimer(Random.Range(minTimeSpawn, maxTimeSpawn), Spawn);
     }
 }
